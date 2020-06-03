8f3bc7323572a8e8136e2a56bf5dffda9a63ebd7
jayprakash.sftr@yahoo.com <jayprakash.sftr@yahoo.com>
2020-06-03 06:17:05 +0000

    Added unit test project

---
+++ CBA.Training.Talmate.Api.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CBA.Training.Talmate.Api.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
