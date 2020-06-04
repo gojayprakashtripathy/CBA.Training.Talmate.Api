6c5f4c55a3ab762ef849cda079542aa9085ec26a
Theja Bangarupalyam <theja_b@HCL.COM>
2020-06-04 11:52:50 +0530

    added migrations to add new table ,database updated& created services and controller for resouce acess

---
+++ CBA.Training.Talmate.Services/ResourceDetailsservice/IResourceDetailsService.cs
@@ -0,0 +1,16 @@
+﻿using CBA.Training.Talmate.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBA.Training.Talmate.Services.ResourceDetailsservice
+{
+    public interface IResourceDetailsService
+    {
+        Task<IQueryable<ResourceDetail>> Get();
+        Task<bool> Post(ResourceDetail resource);
+        Task<ResourceDetail> GetById(int id);
+    }
+}
